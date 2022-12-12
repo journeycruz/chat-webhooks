936acdbb0444538c708e4a70290fbbe9ca4372b8
Journey Cruz <journey.cruz@gmail.com>
2022-12-11 23:03:33 -0600

    Update dadjokes.js

--- pages/api/dadjokes.js
+++ pages/api/dadjokes.js
@@ -1,13 +1,25 @@
 export default async function handler(req, res) {
   const response = await fetch(`https://dad-jokes.p.rapidapi.com/random/joke`, {
     headers: {
-      'X-RapidAPI-Key': process.env.RAPIDAPI_DADJOKES_KEY,
-      'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com',
+      "X-RapidAPI-Key": process.env.RAPIDAPI_DADJOKES_KEY,
+      "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
     },
-  })
-  const data = await response.json()
+  });
+  const data = await response.json();
 
-  let setup = data.body[0].setup
-  let punchline = data.body[0].punchline
-  res.status(200).json({ text: `\n>>>*${setup}*\n\n_${punchline}_\n\n` })
+  let setup = data.body[0].setup;
+  let punchline = data.body[0].punchline;
+  const post = fetch(
+    "https://her.synology.me:5001/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22VBLsE9N9Z1Tmrhyb7RfJtdFWmiDhqPBfa7EwHLIQn8TJ7tJzX8vJx2WgyERH6odm%22",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: {
+        payload: `\n>>>*${setup}*\n\n_${punchline}_\n\n`,
+      },
+    }
+  );
+  post();
 }
